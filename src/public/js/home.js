cdec136b4af81f85b72839cc3c8d09dbf8a017a1
Nathaniel Campbell <nathanielcampbell2002@gmail.com>
2024-05-15 23:36:08 -0500

    Updated home page

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -4,8 +4,8 @@ const socket = io();
 const openCreateModalBtn = document.getElementById('modal__button--create');
 const openJoinModalBtn = document.getElementById('modal__button--join');
 
-const createModal = document.getElementById('modal_create');
-const joinModal = document.getElementById('modal_join');
+const createModal = document.getElementById('modal--create');
+const joinModal = document.getElementById('modal--join');
 
 // Listen for socket events
 socket.on('error', (message) => {
@@ -13,22 +13,22 @@ socket.on('error', (message) => {
 });
 
 // Setup event listeners
-openCreateModalBtn.addEventListener('click', () => {
+openCreateModalBtn?.addEventListener('click', () => {
 	createModal.showModal();
 	joinModal.close();
 });
-openJoinModalBtn.addEventListener('click', () => {
+openJoinModalBtn?.addEventListener('click', () => {
 	createModal.close();
 	joinModal.showModal();
 });
 
-createModal.addEventListener('close', (e) => {
+createModal?.addEventListener('close', (e) => {
 	// Get reason for closing
 	// If closed by form submit, create a new room
 	// Navigate to new game
 });
 
-joinModal.addEventListener('close', (e) => {
+joinModal?.addEventListener('close', (e) => {
 	// Get reason for closing
 	// If closed by form submit, create a new room
 	// Navigate to new game
